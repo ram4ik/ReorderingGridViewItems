643f24bf3fb5c7f952f087e28096cc7f853e74af
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-01-18 14:14:36 +0200

    Initial Commit

---
+++ ReorderingGridViewItems/ReorderingGridViewItemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReorderingGridViewItemsApp.swift
+//  ReorderingGridViewItems
+//
+//  Created by ramil on 18.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ReorderingGridViewItemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
